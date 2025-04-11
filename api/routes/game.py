571aff4b8659a1b16fad85c1c3e3fc256d5e80ad
Razvan Sauciuc <razvansauciucc@outlook.com>
2025-04-11 17:03:56 +0300

    API fixes.

--- api/routes/game.py
+++ api/routes/game.py
@@ -206,8 +206,8 @@ async def choose_color(request: ChooseColor):
             elif round.player1_choice == "BLUE" and round.player2_choice == "BLUE":
                 round.player1_score -= 6
                 round.player2_score -= 6
-        game.player1_total_score += round.player1_score
-        game.player2_total_score += round.player2_score
+        game.player1_score += round.player1_score
+        game.player2_score += round.player2_score
         session.commit()
         session.refresh(round)
         session.refresh(game)
@@ -242,13 +242,10 @@ async def choose_color(request: ChooseColor):
                     "message": "Game over! All 10 rounds completed.",
                     "player1_choice": round.player1_choice,
                     "player2_choice": round.player2_choice,
-                    "player1_score": round.player1_score,
-                    "player2_score": round.player2_score,
+                    "player1_score": game.player1_score,
+                    "player2_score": game.player2_score,
 
                     "game_state": "finished",
-
-                    "player1_total_score": game.player1_total_score,
-                    "player2_total_score": game.player2_total_score
                 },
             )
     else:
